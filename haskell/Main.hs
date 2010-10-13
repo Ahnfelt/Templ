19f1b1b3fb7fb5c3eda9c2bfd1d470beb349e000
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2010-10-13 17:38:49 +0200

    info

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -41,4 +41,12 @@ main = do
                 infer (EApply (EVariable "#template") (EVariable "#input"))) of
                 scheme@(_, TText) -> return ()
                 scheme@(_, t) -> error ("The result type should be string but was " ++ prettyType scheme)
+        ["info", filename] -> do
+            file <- readFile filename
+            let file' = case parseTextExpression filename file of
+                    Left error' -> error (show error')
+                    Right file' -> file'
+            let file'' = ELambda "data" file'
+            let file''' = runInfer (infer file'')
+            putStrLn (prettyType file''')
 
