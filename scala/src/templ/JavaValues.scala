bc5cd443cfa5b43515c20b81d0c15f408fe19216
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2010-11-15 23:00:27 +0100

    JavaValues for constructing Value instances from any Java object

---
+++ scala/src/templ/JavaValues.scala
@@ -0,0 +1,34 @@
+package templ
+
+import templ.Value._
+import scala.collection.immutable.Map
+import java.lang.reflect.Method
+import java.lang.Iterable
+import scala.collection.JavaConversions._
+
+object JavaValues {
+  def fromObject(value: AnyRef): Value = {
+    value match {
+      case value: String => VText(value)
+      case value: Iterable[AnyRef] => VList(value.map(fromObject _).toList)
+      case value =>
+        val methods = value.getClass.getMethods
+        val getters = methods.filter(isGetter _)
+        val fields = getters.map(method => (label(method), call(value, method)))
+        VRecord(Map(fields: _*))
+    }
+  }
+
+  def isGetter(method: Method): Boolean = {
+    !method.getDeclaringClass.isAssignableFrom(classOf[Object]) &&
+    method.getName().startsWith("get")
+  }
+
+  def call(record: Object, method: Method): Value = {
+    fromObject(method.invoke(record))
+  }
+
+  def label(method: Method): String = {
+    method.getName().substring(3, 4).toLowerCase + method.getName().drop(4)
+  }
+}
--- scala/src/templ/Test.scala
+++ scala/src/templ/Test.scala
@@ -2,8 +2,14 @@ package templ
 
 import templ.Expression._
 import templ.Value._
+import reflect.BeanProperty
+import java.util.{Collections, Arrays}
 
 object Test extends Application {
+  class Person {
+    def getName = "Peter"
+    def getEmail = "peter@gmail.com"
+  }
 
   val e = EChoice(ELookup(ERecord(Map(("x", (EText("first"), true)))), "x"), EText("second"))
   val v = Interpreter.interpret(e, Map(), ("", 0, 0))
@@ -14,6 +20,8 @@ object Test extends Application {
   println(result.get)
   println(Interpreter.interpret(result.get, Map(), ("", 0, 0)))
 
+  println(JavaValues.fromObject("foo"))
+  println(JavaValues.fromObject(Collections.singletonList("bar")))
+  println(JavaValues.fromObject(new Person))
   //print("Hello World!1" + " Another string")
-  
 }
