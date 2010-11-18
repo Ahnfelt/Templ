762b1a0f431c84273a391da7dff66d3dd4ad8299
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2010-11-18 12:51:47 +0100

    Added list operations: first, last, front and back

--- templ/src/main/scala/templ/Test.scala
+++ templ/src/main/scala/templ/Test.scala
@@ -11,17 +11,38 @@ object Test extends Application {
     def getEmail = "peter@gmail.com"
   }
 
-  val e = EChoice(ELookup(ERecord(Map(("x", (EText("first"), true)))), "x"), EText("second"))
-  val v = Interpreter.interpret(e, Map(), ("", 0, 0))
-  println(v)
+  {
+    val e = EChoice(ELookup(ERecord(Map(("x", (EText("first"), true)))), "x"), EText("second"))
+    val v = Interpreter.interpret(e, Map(), ("", 0, 0))
+    println(v)
+  }
 
-  val program = "@for $x [{a}, {b}, {c}] {$x}"
-  val result = Parser.parseAll(Parser.textExpression, program)
-  println(result.get)
-  println(Interpreter.interpret(result.get, Map(), ("", 0, 0)))
+  {
+    val program = "@for $x [{a}, {b}, {c}] {$x}"
+    val result = Parser.parseAll(Parser.textExpression, program)
+    println(result.get)
+    println(Interpreter.interpret(result.get, Map(), ("", 0, 0)))
+  }
 
-  println(JavaValues.fromObject("foo"))
-  println(JavaValues.fromObject(Collections.singletonList("bar")))
-  println(JavaValues.fromObject(new Person))
-  //print("Hello World!1" + " Another string")
+  {
+    println()
+    def run(program: String) {
+      val result = Parser.parseAll(Parser.textExpression, program)
+      println(program)
+      println(result.get)
+      println(Interpreter.interpret(result.get, Map(), ("", 0, 0)))
+      println()
+    }
+    run("@first [{a}, {b}, {c}]")
+    run("@last [{a}, {b}, {c}]")
+    run("@front [{a}, {b}, {c}]")
+    run("@back [{a}, {b}, {c}]")
+    run("@first [] | No elements")
+  }
+
+  {
+    println(JavaValues.fromObject("foo"))
+    println(JavaValues.fromObject(Collections.singletonList("bar")))
+    println(JavaValues.fromObject(new Person))
+  }
 }
